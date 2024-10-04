59ba66e55b662aa58af3cbd8dc666269333ee7b6
peter guan <pguan4@illinois.edu>
2024-10-03 19:23:56 -0500

    X-Code Files added.

---
+++ QuizCraft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuizCraft
+//
+//  Created by peter guan on 10/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!11")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuizCraft/QuizCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizCraftApp.swift
+//  QuizCraft
+//
+//  Created by peter guan on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
